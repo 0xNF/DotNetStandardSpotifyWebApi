bf95c692320046601d33af1328df298be11de012
0xnf <nf@Yggdrasil.localdomain>
2017-05-25 10:07:07 -0700

    namespace minor changes

--- AuthorizationCodeFlow.cs
+++ AuthorizationCodeFlow.cs
@@ -52,29 +52,28 @@ namespace DotNetStandardSpotifyWebApi {
 
         //Returns a SpotifyOAuthCredentials. On success, returns with an access token, a refresh token, and a timer limit for the validity of the access token. (Refresh has no expiry). Sets WasSuccessful to True
         //On error, returns with the error code and message, and the other fields are null. Also sets WasSuccess to false.
-        public static async Task<SpotifyOAuthCredentials> GetSpotifyTokenCredentials(string OAuthToken, string clientId, string clientSecret, string redirectUri){
+        public static async Task<OAuthCredentials> GetSpotifyTokenCredentials(string OAuthToken, string clientId, string clientSecret, string redirectUri){
             //Uses System.Net; System.Net.Http; System.Net.Http.Headers;
             string postBody = $"code={OAuthToken}&redirect_uri={redirectUri}&grant_type=authorization_code";
             return await GetSpotifyCredentials(postBody, clientId, clientSecret);        
         }
 
-        public static async Task<SpotifyOAuthCredentials> RefreshAccessToken(SpotifyOAuthCredentials Credentials, string clientId, string clientSecret){
-            return await RefreshAccessToken(Credentials.refresh_token, clientId, clientSecret);
+        public static async Task<OAuthCredentials> RefreshAccessToken(OAuthCredentials Credentials, string clientId, string clientSecret){
+            return await RefreshAccessToken(Credentials.Refresh_token, clientId, clientSecret);
         }
 
-        public static async Task<SpotifyOAuthCredentials> RefreshAccessToken(string refreshToken, string clientId, string clientSecret){
+        public static async Task<OAuthCredentials> RefreshAccessToken(string refreshToken, string clientId, string clientSecret){
             string postBody = $"grant_type=refresh_token&refresh_token={refreshToken}";
             return await GetSpotifyCredentials(postBody, clientId, clientSecret, refreshToken);                         
         }
 
         #region private helpers
-        private static async Task<SpotifyOAuthCredentials> GetSpotifyCredentials(string PostBody, string clientId, string clientSecret, string refreshToken = null){
+        private static async Task<OAuthCredentials> GetSpotifyCredentials(string PostBody, string clientId, string clientSecret, string refreshToken = null){
             string b64 = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{clientId}:{clientSecret}"));
             HttpRequestMessage request = WebRequestHelpers.SetupRequest(SpotifyAPITokenUrl, "Basic", b64, HttpMethod.Post);
             HttpContent content = new StringContent(PostBody, Encoding.UTF8, "application/x-www-form-urlencoded");
             request.Content = content;
             HttpResponseMessage response = await WebRequestHelpers.Client.SendAsync(request);
-            SpotifyOAuthCredentials credentials = new SpotifyOAuthCredentials();
             if(response.IsSuccessStatusCode){
                 string jsonstring = await response.Content.ReadAsStringAsync();
                 JObject jobj = Newtonsoft.Json.Linq.JObject.Parse(jsonstring);
@@ -83,9 +82,11 @@ namespace DotNetStandardSpotifyWebApi {
                 if(string.IsNullOrWhiteSpace(refreshToken)){
                     refreshToken = jobj.GetValue("refresh_token").ToString();
                 }
-                credentials = new SpotifyOAuthCredentials("unknown user", accessToken, expiresIn, refreshToken);
+                return new OAuthCredentials("unknown user", accessToken, expiresIn, refreshToken);
             }
-            return credentials;                      
+            else {
+                return OAuthCredentials.CrendentialError;
+            }
         }
         #endregion
     }
---
+++ Helpers/OAuthCredentials.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetStandardSpotifyWebApi.Helpers
+{
+    /// <summary>
+    /// Holds authenticated OAuth2 credentials, including an access token, refresh token, expiry time, and whether the response was an error.
+    /// </summary>
+    public sealed class OAuthCredentials {
+        /// <summary>
+        /// Optional UserId for services that provide user ids
+        /// </summary>
+        public string UserId { get; } = string.Empty;
+        /// <summary>
+        /// OAuth2 Access Token
+        /// Typically expires after a set period time, defined by Expires_in
+        /// </summary>
+        public string Access_token { get; } = string.Empty;
+        /// <summary>
+        /// Time in seconds that an OAuth Access Token expires
+        /// </summary>
+        public int Expires_in { get; } = -1;
+        /// <summary>
+        /// Refresh token that can be used to obtain a new Access Token. 
+        /// Often has no expiry
+        /// </summary>
+        public string Refresh_token { get; } = string.Empty;
+
+        /// <summary>
+        /// True if the server response was an error
+        /// </summary>
+        public bool WasError { get; } = true;
+
+        /// <summary>
+        /// Error class - use if the server returned invalid credentials
+        /// </summary>
+        public static OAuthCredentials CrendentialError { get; } = new OAuthCredentials("Error", "Error", -1, "Error");
+
+        public OAuthCredentials(string userid, string token, int expires, string refresh) {
+            this.UserId = userid;
+            this.Access_token = token;
+            this.Expires_in = expires;
+            this.Refresh_token = refresh;
+            this.WasError = false;
+        }
+
+        public OAuthCredentials() {
+
+        }
+    }
+}
