1ea162239de16473e1988e36452184080be0ebbf
0xnf <nf@Yggdrasil.localdomain>
2017-06-01 09:57:44 -0700

    removed api keys from file, and regenerated app secret.

--- DotNetStandardSpotifyWebApi.Tests/AuthorizationTests.cs
+++ DotNetStandardSpotifyWebApi.Tests/AuthorizationTests.cs
@@ -11,17 +11,11 @@ namespace DotNetStandardSpotifyWebApi.Tests
         private const string fakeClientSecret = "fakeSecret";
         private const string fakeRedirectUrl = "fakeUri";
 
-        //Dummy app with full permissions for API testing purposes
-        private const string Client_Id = "293676b585fa44739c6f334c184d6e96";
-        private const string Client_Secret = "e0f16720c65e4298ba505abd37646434";
-        private static string RefreshToken {get; set;}
-        private static string AccessToken {get; set;}
-
-        [Fact]
-        public async void ShouldFailToAuthorize(){
-            OAuthCredentials token = await AuthorizationCodeFlow.GetSpotifyTokenCredentials(FakeOAuthCode, FakeClientId, fakeClientSecret, fakeRedirectUrl);
-            Assert.True(token.WasError, "Given false authorization codes, auth should fail");
-        }
+        // [Fact]
+        // public async void ShouldFailToAuthorize(){
+        //     OAuthCredentials token = await AuthorizationCodeFlow.GetSpotifyTokenCredentials(FakeOAuthCode, FakeClientId, fakeClientSecret, fakeRedirectUrl);
+        //     Assert.True(token.WasError, "Given false authorization codes, auth should fail");
+        // }
 
         // [Fact]
         // public async void ShouldSuccessfullyReturnOAuthToken(){
